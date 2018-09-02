542fe67e76203e93795896a0d287f257afa7a7c4
weiyueying <493454974@qq.com>
2018-09-02 11:12:02 +0800

    第一次配置项

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+// 导入全局样式
+// 导入bootstrap的样式
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './assets/css/index.css';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
