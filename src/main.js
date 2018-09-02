5ae2a6bb9a24761a4f987195ba9d4ff25ffa1981
weiyueying <493454974@qq.com>
2018-09-02 14:31:58 +0800

    路由组件进一步完成

--- src/main.js
+++ src/main.js
@@ -9,15 +9,23 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
 import VueRouter from 'vue-router';
 
+import Heroslist from './views/heros/list.vue';
+import Weaponslist from './views/weapons/list.vue';
+import Equipslist from './views/equips/list.vue';
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+    linkExactActiveClass: 'active',
     routes: [
-
+        { path: '/heros', component: Heroslist },
+        { path: '/weapons', component: Weaponslist },
+        { path: '/equips', component: Equipslist }
     ]
 })
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
