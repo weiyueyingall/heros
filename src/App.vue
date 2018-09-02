66c4b5c23b0019c5570c698160f719e1469f518b
weiyueying <493454974@qq.com>
2018-09-02 11:26:00 +0800

    app-header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="myapp">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- 头部 -->
+    <app-header></app-header>
     <div class="container-fluid">
     <div class="row">
       <div class="col-sm-3 col-md-2 sidebar">
@@ -242,8 +231,13 @@
 </template>
 
 <script>
+// 导入AppHeader组件
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'app',
+  components:{
+      AppHeader
+  },
   data () {
     return {
      
